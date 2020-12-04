85bcf3b3d496c6f0286a1deb6ece861b6a3306ea
JohnWHirsch <johnwhirsch@gmail.com>
2020-12-04 15:49:31 -0500

    Create get-dnsclients.ps1

---
+++ get-dnsclients.ps1
@@ -0,0 +1,43 @@
+param([Parameter(Mandatory=$True)][string]$logfile)
+
+if($logfile -like "\\*"){ $log = Get-Content "Microsoft.PowerShell.Core\FileSystem::$($logfile)" }
+else{ $log = Get-Content $logfile }
+
+$dnsclients = @()
+
+foreach($line in $log){
+    
+    if($line -match "(UDP Rcv [0-9\.]+[A-Za-z0-9\s]+[\[\]A-Z0-1\s]+[\(\)\-A-Za-z0-9]+)"){ 
+    
+        $datapattern = '(UDP Rcv )([0-9\.]+)(\s+[A-Za-z0-9]+\s+)([A-Za-z]?\s+[A-Za-z])(\s)(\[[A-Za-z0-1\s]+\])(\s+)([A-Za-z]+)(\s+)([\(\)\-`_A-Za-z0-9]+)'
+
+        $linedata = [regex]::Matches($line, $datapattern)
+
+        # Leaving these here for future additions to the script(maybe)
+        # Additional info can be added to the propsout array if desired
+
+        $PacketType          = $linedata.Groups[1]
+        $ClientIP            = $linedata.Groups[2]
+        $QueryType           = $linedata.Groups[4]
+        $QueryResponseStatus = $linedata.Groups[6]
+        $QueryResponseType   = $linedata.Groups[8]
+        $Query               = ($linedata.Groups[10] -replace "(\([0-9]+\))", ".").TrimStart(".")
+
+        $addIP = $true
+
+        foreach($existingclient in $dnsclients){ if($ClientIP -match $($existingclient.ClientIP)){ $addIP = $false } }
+
+        if($addIP){
+            write-host "Found new client $ClientIP"
+            $ClientHostname = ([system.net.dns]::GetHostByAddress($ClientIP)).hostname
+            $propsout            = @{
+                                    ClientIP        = $ClientIP
+                                    ClientHostname  = $ClientHostname
+                                 }
+            $objout              = New-Object -TypeName psobject -Property $propsout 
+            $dnsclients          += $objout               
+        }            
+    }
+}
+
+$dnsclients | Sort-Object ClientHostname | FT
